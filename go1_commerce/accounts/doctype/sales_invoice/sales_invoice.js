dc051d1c5abdb9431c2e83d95f8d335b20438bcc
TridotsTech <info@tridotstech.com>
2024-08-02 10:59:34 +0530

    fix: Enabled sales invoice script for order doctype.

--- go1_commerce/accounts/doctype/sales_invoice/sales_invoice.js
+++ go1_commerce/accounts/doctype/sales_invoice/sales_invoice.js
@@ -1,8 +1,52 @@
-// Copyright (c) 2024, Tridotstech PVT LTD and contributors
+// Copyright (c) 2020, Tridots Tech and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Sales Invoice", {
-// 	refresh(frm) {
+frappe.ui.form.on('Sales Invoice', {
+	refresh: function(frm) {
+		if(frm.doc.status!="Paid" && frm.doc.docstatus==1){
+            frm.remove_custom_button('Make Payment');
+            frm.add_custom_button("Make Payment", function() {
+                frappe.model.open_mapped_doc({
+                    method: "go1_commerce.accounts.api.make_invoice_payment",
+                    frm: cur_frm
+                })
+            });
+        }  
+        if (frm.doc.tax_breakup) {
+            frm.trigger('show_tax_spliup')
+        }     
+	},
 
-// 	},
-// });
+	show_tax_spliup: function(frm) {
+        let wrapper = $(frm.get_field('tax_splitup').wrapper).empty();
+        let table_html = $(`<table class="table table-bordered" style="cursor:pointer; margin:0px;">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 70%">Tax</th>
+                                        <th>Tax %</th>
+                                        <th>Tax Amount</th>
+                                        <th>Tax Type</th>
+                                    </tr>
+                                </thead>
+                                <tbody></tbody>
+                            </table>`).appendTo(wrapper);
+        let tax = frm.doc.tax_breakup.split('\n');
+        if (tax.length > 0) {
+            tax.map(f => {
+                if (f) {
+                    let tax_type = f.split('-')[0].trim();
+                    let tax_percent = f.split('-')[1].trim();
+                    let tax_value = f.split('-')[2].trim();
+                    let product_tax_type = f.split('-')[3];
+                    let row_data = $(`  <tr>
+                                            <td> ${tax_type} </td>
+                                            <td> ${frappe.format(tax_percent)} </td>
+                                            <td> ${parseFloat(tax_value).toFixed(2)} </td>
+                                            <td> ${product_tax_type} </td>
+                                        </tr>`);
+                    table_html.find('tbody').append(row_data);
+                }
+            })
+        }
+    },
+});
--- go1_commerce/accounts/doctype/sales_invoice/sales_invoice.py
+++ go1_commerce/accounts/doctype/sales_invoice/sales_invoice.py
@@ -1,9 +1,40 @@
-# Copyright (c) 2024, Tridotstech PVT LTD and contributors
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Tridots Tech and contributors
 # For license information, please see license.txt
 
-# import frappe
+from __future__ import unicode_literals
+import frappe
 from frappe.model.document import Document
-
+from frappe import _
+from go1_commerce.utils.setup import get_settings_value, get_settings
 
 class SalesInvoice(Document):
-	pass
+	def autoname(self): 
+		naming_series='INV-.YYYY.-'
+		self.naming_series=naming_series
+		from frappe.model.naming import make_autoname
+		self.name = make_autoname(naming_series+'.#####', doc=self)
+
+	def on_submit(self):
+		from go1_commerce.accounts.api import make_invoice_payment
+		if frappe.db.exists("Order", self.reference):
+			doc = frappe.get_doc("Order", self.reference)
+		if doc.payment_status=="Paid":
+			order_settings = get_settings('Order Settings')
+			if order_settings.automate_invoice_creation==1:
+				make_invoice_payment(source_name=None, source=self.name)
+
+	def on_update_after_submit(self):
+		if self.reference and self.status=="Paid":
+			if frappe.db.exists("Order", self.reference):
+				doc = frappe.get_doc("Order", self.reference)
+				doc.payment_status = "Paid"
+				outstanding_amount = self.total_amount - self.paid_amount
+				doc.paid_amount = self.paid_amount
+				self.outstanding_amount = outstanding_amount
+				doc.db_update()
+				frappe.db.sql('''UPDATE `tabOrder` SET outstanding_amount = %(outstanding_amount)s , 
+							  payment_status= %(payment_status)s WHERE name = %(doc_name)s''',
+							  {"payment_status":"Paid", "doc_name":self.reference, "outstanding_amount": outstanding_amount})
+				frappe.db.commit()
+				res = frappe.db.sql(f"""SELECT * FROM `tabOrder` WHERE name = ''{self.reference}""")
