cf5f94d0fa57543f020b2a4f5c3796bbf298a4ec
TridotsTech <info@tridotstech.com>
2024-08-06 10:43:20 +0530

    fix: Email templated updated.

--- go1_commerce/go1_commerce/doctype/campaign/campaign.py
+++ go1_commerce/go1_commerce/doctype/campaign/campaign.py
@@ -1,9 +1,16 @@
-# Copyright (c) 2024, Tridotstech PVT LTD and contributors
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, info@valiantsystems.com and contributors
 # For license information, please see license.txt
 
-# import frappe
+from __future__ import unicode_literals
+import frappe
 from frappe.model.document import Document
 
-
 class Campaign(Document):
-	pass
+    def on_update(self):
+        if self.get("campaign_schedules"):
+            for ecmp in self.campaign_schedules:
+                for k in frappe.db.get_all ("Email Campaign",{"campign_id":ecmp.name,"status":["IN",["Scheduled","In Progress"]],"email_template":["!=",ecmp.email_template]},["name"]):
+                    doc = frappe.get_doc("Email Campaign",k.name)
+                    doc.email_template = ecmp.email_template
+                    doc.save(ignore_permissions = True)
